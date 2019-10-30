87fffc6a4f7ab7b7fad6f6519893c62825e580e2
urabor <urobores1600@gmail.com>
2019-10-30 18:17:24 +0400

    7q

--- src/comps/MainFormComponents/MFCoord.js
+++ src/comps/MainFormComponents/MFCoord.js
@@ -13,28 +13,29 @@ function Coord(){
             <div className='coord-elem'>
                 <p>Вырезанный фрагмент</p>
                 <div className='coord-head-left'>
-                     <Button style={{width:'5%'}}><Icon type='undo'/></Button>
-                     <Button style={{width:'5%'}}><Icon type='redo'/></Button>
-                     <Button style={{width:'15%'}}>Распознать</Button>
+                     <Button style={{width:'45px', marginRight: '7px'}}><Icon type='undo'/></Button>
+                     <Button style={{width:'45px'}}><Icon type='redo'/></Button>
+                     <Button style={{width:'150px', position:'absolute', right: '0%'}}>Распознать</Button>
                 </div>
                     <Dragger {...props} className='coord-drager'>
                         <p className="ant-upload-drag-icon">
                         <Icon type="inbox" />
                         </p>
-                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                        <p className="ant-upload-hint">
+                        <p className="ant-upload-text">Перетащите сюда изображение таблицы координат, которое
+                        надо распознать или вырежьте его из оригинала акта ЛПО</p>
+                        {/* <p className="ant-upload-hint">
                         Support for a single or bulk upload. Strictly prohibit from uploading company data or other
                         band files
-                        </p>
+                        </p> */}
                     </Dragger>
             </div>
             <div className='coord-elem'>
                 <p>Результат распознования</p>
                 <div className='coord-head-left'>
-                     <p>Тип координат Руибы/Линии</p>
-                     <Button style={{width:'25%'}}>Записать в таблицу</Button>
+                     <p>Тип координат Румбы/Линии</p>
+                     <Button style={{width:'150px', position:'absolute', right:'0%'}}>Записать в таблицу</Button>
                 </div>
-                <div className="coord-result">
+                <div className="coord-result" style={{marginRight: '0px'}}>
                     <p>Результат распознавания появиться здесь, после нажатия кнопки "Распознать"</p>
                 </div>
             </div>
--- src/comps/MainFormComponents/MFTableCoord.js
+++ src/comps/MainFormComponents/MFTableCoord.js
@@ -1,12 +1,242 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Menu, Icon, Button } from 'antd';
+import { Menu, Icon, Button, Table, Select, Popconfirm } from 'antd';
 
-function TableCoord(){
+const { Option } = Select;
+
+class TableCoord extends React.Component{
+    constructor(props){
+        super(props);
+        this.columns1 = [
+            
+            {
+                title:'##',
+                dataIndex: 'num',
+            },
+            {
+              title: 'Широта',
+              dataIndex: 'shirota',
+            },
+            {
+                title: 'Долгота',
+                dataIndex: 'dolgota',
+              },
+              {
+                title: 'Инструменты',
+                dataIndex: 'tools',
+                render: (text, record ) => (
+                    <span style={{position: 'absolute', right: '20%'}}>
+                      <a  style={{padding: '8px'}} onClick={this.addRow} >
+                           <Icon type="plus" />
+                           </a>
+                      <a  ><Popconfirm title="Sure to delete?" onConfirm={() =>  this.deleteRow(record.key)} >
+                          
+                          <Icon type="delete" /></Popconfirm>
+                      </a>
+                    </span>
+                  )
+              },
+          ];
+          this.columns2 = [
+            
+            {
+                title:'##',
+                dataIndex: 'num',
+            },
+            {
+              title: 'Шир',
+              dataIndex: 'shirota',
+            },
+            {
+                title: 'Бегинс',
+                dataIndex: 'dolgota',
+              },
+              {
+                title: 'flex',
+                dataIndex: 'flex',
+              },
+              {
+                title: 'Инструменты',
+                dataIndex: 'tools',
+                render: (text, record ) => (
+                    <span style={{position: 'absolute', right: '20%'}}>
+                      <a  style={{padding: '8px'}} onClick={this.addRow} >
+                           <Icon type="plus" />
+                           </a>
+                      <a  ><Popconfirm title="Sure to delete?" onConfirm={() =>  this.deleteRow(record.key)} >
+                          
+                          <Icon type="delete" /></Popconfirm>
+                      </a>
+                    </span>
+                  )
+              },
+          ];
+      
+          this.state1 = 
+          {
+              data:
+              [
+                  {
+                  key: '1',
+                  num: 1,
+                  shirota: 33.5,
+                  dolgota: 32,
+                  flex: '',
+                  tools: ''
+                  },
+                  {
+                    key: '2',
+                    num: 2,
+                    shirota: 33.5,
+                    dolgota: 32,
+                    flex: '',
+                    tools: ''
+                    },
+              ],
+              count:2,
+              newData: {
+                key: 9,
+                num: 0,
+                shirota: 33.5,
+                dolgota: 32,
+                flex: '',
+                tools: ''
+              },
+      };
+      this.state2 = 
+          {
+              data:
+              [
+                  {
+                  key: '1',
+                  num: 1,
+                  shirota: 33.5,
+                  dolgota: 32,
+                  tools: ''
+                  },
+                  {
+                    key: '2',
+                    num: 2,
+                    shirota: 33.5,
+                    dolgota: 32,
+                    tools: ''
+                    },
+              ],
+              count:2,
+              newData:`{
+                key: this.state2.count,
+                num: this.state2.count,
+                shirota: 33.5,
+                dolgota: 32,
+                tools: ''
+              }`
+            ,
+      };
+      this.current= 'gg';
+    }
+      deleteRow = key =>{
+        const dataSource = [...this.state.data];
+        
+        console.log(key)
+        this.setState({ data: dataSource.filter(item => item.key !== key) });
+        
+    };
+    addRow =() => { 
+        let i = 1; let state;
+        if(this.current=='gg'){
+            i=1; state = this.state1;
+        }
+        if(this.current=='mm'){
+            i=2; state = this.state2;
+        }
+        const {count, data}= state;
+        
+        // const newData = Object.assign({},this[`state${i}`].newData);
+        const newData = Object.assign({},state.newData);
+            this.setState({  data: [...data, newData],
+            count: count+1,  }   );
+    };
+
+
+    handleClick = e => {
+        
+        // this.setState({
+        //   current: e.key,
+        // });
+        this.current = e.key;
+        // console.log(this.dataRef)
+      };
+    render(){
+        let colomnRef;
+        let dataRef;
+        let dataR= ()=>{
+        if(this.current==='gg'){
+            this.colomnRef=this.columns1;
+            this.dataRef=this.state1.data;
+            console.log(this.colomnRef)
+        }
+        else if(this.current==='mm'){
+            this.colomnRef=this.columns2;
+            this.dataRef=this.state2.data;
+        }
+        else if(this.current==='az'){
+            colomnRef=this.columns1;
+            dataRef=this.state1.data;
+        }
+        else if(this.current==='rum'){
+            colomnRef=this.columns2;
+            dataRef=this.state2.data;
+        }
+    };
+
+        const columns = colomnRef.map(col => {
+                if (!col.editable) {
+                  return col;
+                }
+                return {
+                  ...col,
+                  onCell: record => ({
+                    record,
+                    editable: col.editable,
+                    dataIndex: col.dataIndex,
+                    title: col.title,
+                  })
+                };
+              });
+      
+
+        //  columns = this.columns.map(col => {
+        //     if (!col.editable) {
+        //       return col;
+        //     }
+        //     return {
+        //       ...col,
+        //       onCell: record => ({
+        //         record,
+        //         editable: col.editable,
+        //         dataIndex: col.dataIndex,
+        //         title: col.title,
+        //       })
+        //     };
+        //   });
     return(
         <div>
+            <Menu onClick={this.handleClick, this.dataR} selectedKeys={[this.current]} mode='horizontal'>
+                <Menu.Item key='gg'>ГГ, ГГГГГ</Menu.Item>
+                <Menu.Item key='mm'>ГГ ММ СС.с</Menu.Item>
+                <Menu.Item key='az'>Азимуты</Menu.Item>
+                <Menu.Item key='rum'>Румбы</Menu.Item>
+            </Menu>
+            <Table  columns={columns} dataSource={dataRef} pagination={false} bordered={true} size={'small'}>
 
+            </Table>
+            <Button
+                onClick={this.addRow}
+               type="primary" style={{ marginBottom: 16 }}>
+                + Добавить строку
+            </Button>
         </div>
     )
+}
 }
 export default TableCoord
